dba4c779065c1fd379144f0b7902373811da3952
z21ite09 <z21ite09@z00p3-14.SPSKNM>
2021-09-30 14:31:47 +0200

    2.Hodina

---
+++ 2.Hodina/main.py
@@ -0,0 +1,14 @@
+username=input("Zadaj Prihlasovacie meno")
+password=input("Zadaj prihlasovacie heslo")
+vek= input("Zadaj svoj vek")
+
+if username == "root" and password == "password" or vek== 18:
+    print("Úspešne si sa prihlasil")
+else:
+    print("Zlé meno alebo heslo alebo vek")
+
+
+
+
+
+
